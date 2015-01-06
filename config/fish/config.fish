e299001e3eca547169744f203fc7dede46ae39a7
Matthew Seiler <theflyinghyrax@gmail.com>
2015-01-05 20:07:04 -0500

    Possibly fixed keychain startup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,14 @@
-# startup keychain to manage ssh-add and ssh-agent
-eval (keychain --eval --agents ssh --quick --quiet id_rsa)
+# startup keychain to manage SSH keys #
+# This: `eval (keychain --eval --agents ssh --quick --quiet id_rsa)`
+# will set SSH_AGENT_PID and SSH_AUTH_SOCKET to garbage,
+# so we have to do things a little differently:
+if status --is-interactive
+    keychain --agents ssh --quick id_rsa
+    set kcout "$HOME/.keychain/"(uname -n)"-fish"
+    if test -e $kcout
+        source $kcout
+    end
+end
 
 # Default todo.txt cli option
 set -x TODOTXT_DEFAULT_ACTION list
