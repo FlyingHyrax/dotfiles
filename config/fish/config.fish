da5be36afde8e7d6546ada29ab65bee0c2ada153
Matthew Seiler <theflyinghyrax@gmail.com>
2015-05-12 09:21:37 -0400

    Don't need keychain on Mac

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,13 +2,15 @@
 # This: `eval (keychain --eval --agents ssh --quick --quiet id_rsa)`
 # will set SSH_AGENT_PID and SSH_AUTH_SOCKET to garbage,
 # so we have to do things a little differently:
-if status --is-interactive
-    keychain --agents ssh --quick id_rsa
-    set kcout "$HOME/.keychain/"(uname -n)"-fish"
-    if test -e $kcout
-        source $kcout
-    end
-end
+
+# Don't need keychain on Mac
+# if status --is-interactive
+#     keychain --agents ssh --quick id_rsa
+#     set kcout "$HOME/.keychain/"(uname -n)"-fish"
+#     if test -e $kcout
+#         source $kcout
+#     end
+# end
 
 # Default todo.txt cli option
 set -x TODOTXT_DEFAULT_ACTION list
